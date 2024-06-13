fd9f45eb1f9be1781e624bebf243519c52864b5c
Jagadesh32 <vsjagadeesh32@gmail.com>
2024-06-13 16:16:55 +0530

    Added print method

--- src/org/gui/Sample.java
+++ src/org/gui/Sample.java
@@ -8,8 +8,19 @@ public class Sample {
 		System.out.println(a);
 	}
 	
+	private void print() {
+		
+		int b = 10;
+		System.out.println(b);
+	}
+	
+	
 	public static void main(String[] args) {
 		
+		Sample s = new Sample();
+		
+		s.add();
+		s.print();
 		
 	}
 
